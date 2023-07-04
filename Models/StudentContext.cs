88d3dce6fc611380f1280ac0e2afe1c6b6e5f765
Krzysztof Iwański <Krzysztof.Iwanski@microsoft.wsei.edu.pl>
2023-07-04 18:54:30 +0200

    Add project files.

---
+++ Models/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiStudent.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiStudent.Models
+{
+    public class StudentContext : /*DbContext */IdentityDbContext<UserEntity, UserRole, int>
+    {
+        public StudentContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Student> Studnets { get; set; }
+    }
+}
